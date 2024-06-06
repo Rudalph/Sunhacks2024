5d2487a8b266e2fa8899b029a5d6b3d1e97c33fe
Rudalph <gonsalvesrudalph@gmail.com>
2024-06-06 21:15:06 +0530

    updates

--- Components/Imgcards.jsx
+++ Components/Imgcards.jsx
@@ -1,7 +1,7 @@
 "use client"
 import * as React from "react"
 
-import { Button } from "@/Components/ui/button"
+import { Button } from "@/components/ui/button"
 import {
   Card,
   CardContent,
@@ -9,16 +9,16 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/Components/ui/card"
-import { Input } from "@/Components/ui/input"
-import { Label } from "@/Components/ui/label"
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
 import {
   Select,
   SelectContent,
   SelectItem,
   SelectTrigger,
   SelectValue,
-} from "@/Components/ui/select"
+} from "@/components/ui/select"
 
 import Image from "next/image"
 
